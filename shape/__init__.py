fb1016efd5bbcab174afe634ef0bb6b2ab7b0e72
Robinpreet kaur <Rrobinpreetkaur30@rrc.ca>
2025-09-26 12:15:23 -0500

    imported the shape.

---
+++ shape/__init__.py
@@ -0,0 +1,10 @@
+"""
+Shape Package Initialization
+----------------------------
+Imports Shape, Triangle, and Rectangle classes.
+"""
+from shape import *
+from shape.triangle import Triangle
+from shape.rectangle import Rectangle
+
+__all__ = ["Shape", "Triangle", "Rectangle"]
